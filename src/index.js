0e8313921e2c7b58248fd3712649dd67ff54cf9d
Sviatoslav-fls <msv.fls@gmail.com>
2021-08-07 13:24:02 +0300

    feat: solution min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  console.log(typeof array)
+  if (!array || !array.length) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array)
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || !array.length) {
+    return 0;
+  } else {
+    return Math.max(...array)
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || !array.length) {
+    return 0;
+  } else {
+    return array.reduce((a, b) => (a + b)) / array.length;
+  }
 }
